c16f3e5194d5e981a78cf310baf7141c745f562c
Sakil <sakilcse91@gmail.com>
2026-09-11 12:44:22 +0600

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
 
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: '/Counter-APP/',
+})
